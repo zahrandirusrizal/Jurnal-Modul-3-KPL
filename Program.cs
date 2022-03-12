9a0d3715ede3e6179241cd33ab9db694bf902197
Zahrandi Rusrizal <Zahrandi@student.telkomuniversity.ac.id>
2022-03-12 09:34:01 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+namespace JURNALMOD3
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+
+        }
+    }
+
+}
